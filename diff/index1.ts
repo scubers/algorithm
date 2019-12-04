fb7a841c145840d0b25418f49b89d8dfe7a3eca3
Jrwong <jr-wong@qq.com>
2019-12-04 15:59:11 +0800

    init

---
+++ diff/index1.ts
@@ -0,0 +1,125 @@
+
+class Diff1 {
+
+    constructor(src: string, dest: string) {
+        this.src = src
+        this.dest = dest
+    }
+    // 原始
+    src: string
+    // 编辑后
+    dest: string
+
+    check() {
+
+        let sourceCount = src.length
+        let destCount = dest.length
+
+        let srcArray = this.src.split("")
+        let destArray = this.dest.split("")
+
+        // 保存对应 k 上的 x 值
+        let map = new Map<number, number>()
+        // 初始化 0 步操作的时候，所在的 x 值为0
+        map.set(0, 0)
+        // 最多执行 n + m 步后能完成更改，所以从小到大开始遍历可能的步数
+        out: for (var d = 1; d < sourceCount + destCount; d++) {
+            console.log(`===============loop d: ${d}`)
+            /**
+             * 设 k = x - y，最优的编辑方案是优先删除，后增加。
+             * 所以 x 大，则删除得多。
+             * 对可能取值的k进行遍历，因为默认移动不能进行对角线移动，只能x或者y移动，
+             * 导致 k = x - y，的跨度为 2，
+             * 对跨度为2的k值进行遍历
+             */
+            for (var k = -d; k <= d; k += 2) {
+                console.log(`k: ${k}`)
+                // 每次迭代都需要从上次迭代保存的值开始计算
+                var lastX: number
+                var lastY: number
+                var newX: number
+                var newY: number
+                var fromTop: boolean
+                if (k == -d) {
+                    // 必须从 k + 1 到达
+                    lastX = map.get(k + 1)!
+                    lastY = lastX - (k + 1)
+                    fromTop = true
+                } else if (k == d) {
+                    // 其他遍历都可从 k - 1 到达
+                    lastX = map.get(k - 1)!
+                    lastY = lastX - (k - 1)
+                    fromTop = false
+                } else {
+                    //
+                    var topX = map.get(k + 1)!
+                    var topY = topX - (k + 1)
+
+                    var leftX = map.get(k - 1)!
+                    var lfetY = leftX - (k - 1)
+
+                    if (topX != leftX) {
+                        // 如果x不一样，取x较大的，因为已经优先删除了较多的元素
+                        if (topX > leftX) {
+                            lastX = topX
+                            lastY = topY
+                            fromTop = true
+                        } else {
+                            lastX = leftX
+                            lastY = lfetY
+                            fromTop = false
+                        }
+                    } else {
+                        // x 一样取y较大的，因为已经优先添加了较多的元素
+                        if (topY > lfetY) {
+                            lastX = topX
+                            lastY = topY
+                            fromTop = true
+                        } else {
+                            lastX = leftX
+                            lastY = lfetY
+                            fromTop = false
+                        }
+                    }
+
+                }
+
+                console.log(`pre (${lastX}, ${lastY})`)
+                var x = lastX
+                var y = lastY
+                if (fromTop) {
+                    y += 1
+                } else {
+                    x += 1
+                }
+                // 如果存在下一个字符相同，则xy都 + 1，形成对角线
+                while ( y < destCount && x < sourceCount && srcArray[x] == destArray[y]) {
+                    console.log("----------------------------------------")
+                    x += 1
+                    y += 1
+                }
+
+                if (y == destCount && x == sourceCount) {
+                    // 找到答案
+                    console.log(`>>>>>>>>>>>>>>>>>>>>>>>> d: ${d}`)
+                    break out
+                }
+                // 保存当前 k 下的 x 值
+                console.log(`current: (${x}, ${y})\n`)
+                map.set(k, x)
+
+            }
+
+
+        }
+        
+    }
+
+}
+
+var src = "ABCABBA"
+var dest = "CBABAC"
+let start1 = new Date().getMilliseconds()
+new Diff(src, dest).check()
+let end1 = new Date().getMilliseconds()
+console.log(`耗时：${(end - start) / 1000.0}`)
---
+++ diff/move.ts
@@ -0,0 +1,109 @@
+
+class IndexedRecord {
+    oldCount: number = 0
+    newCount: number = 0
+    indexes: Array<number> = []
+}
+
+class Change<T> {
+    from: number = 0
+    to: number = 0
+    value?: T
+    constructor(from: number, to: number, value: T) {
+        this.from = from
+        this.to = to
+        this.value = value
+    }
+}
+
+class MoveDiff {
+
+    src: Array<String> = []
+    dest: Array<String> = []
+    constructor(src: String, dest: String) {
+        this.src = src.split('')
+        this.dest = dest.split('')
+    }
+
+    private map = new Map<String, IndexedRecord>()
+
+    getRecord(key: String): IndexedRecord {
+        var r = this.map.get(key)
+        if (r == undefined) {
+            r = new IndexedRecord()
+            this.map.set(key, r)
+        }
+        return r
+    }
+
+    inserts: Array<Change<String>> = []
+    delete: Array<Change<String>> = []
+    move: Array<Change<String>> = []
+
+    check() {
+        // - 遍历目标，保存记录
+        this.dest.forEach((v, i, a) => {
+            let r = this.getRecord(v)
+            r.newCount += 1
+            // 新数据，表示没有位置
+            r.indexes.push(-1)
+        })
+
+        // - 反向遍历源数据，并记录相应位置
+        if (this.src.length > 0) {
+            for (var i = this.src.length - 1; i >= 0; i--) {
+                let c = this.src[i]
+                let r = this.getRecord(c)
+                r.oldCount += 1
+                r.indexes.push(i)
+            }
+        }
+
+        // - 遍历数据，构造Change
+        this.dest.forEach((v, i, a) => {
+            let r = this.getRecord(v)
+            let index = r.indexes.pop()!
+            if (index < 0) {
+                // 表示旧数据中没有，属于新增
+                this.inserts.push(new Change(0, i, v))
+            } else if (index != i) {
+                // } else {
+                // 表示旧数据中有，属于move
+                this.move.push(new Change(index, i, v))
+            }
+        })
+
+        this.src.forEach((v, i, a) => {
+            let r = this.getRecord(v)
+            let index = r.indexes.pop()!
+            if (index > 0) {
+                // 表示新数据没有，属于删除
+                this.delete.push(new Change(0, i, v))
+            }
+        })
+    }
+}
+
+var a = "abcdeft"
+var b = "acdehl"
+
+let diff = new MoveDiff(a, b)
+let s = new Date().getMilliseconds()
+diff.check()
+let e = new Date().getMilliseconds()
+
+
+console.log("-----insert")
+diff.inserts.forEach((v, i, a) => {
+    console.log(`${v.to}: ${v.value}`)
+})
+console.log("-----delete")
+diff.delete.forEach((v, i, a) => {
+    console.log(`${v.to}: ${v.value}`)
+})
+console.log("-----move")
+diff.move.forEach((v, i, a) => {
+    console.log(`${v.value}: ${v.from} -> ${v.to}`)
+})
+
+console.log(`cost: ${e - s} ms`)
